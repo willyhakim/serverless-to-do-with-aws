d9ddf6a7c8a1faa074074d15223646131222080b
Willy Hakizimana <willy.hakizimana@johnsonfit.com>
2018-06-11 16:28:56 -0500

    Add express routing as a proxy to tha api gateway

---
+++ src/api/api.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({message: 'ok'})
+})
+
+app.get('/todos', (req, res) => {
+    const todos = [
+        {
+            id: 1,
+            name: 'clean up',
+            status: 'open'
+        }, {
+            id: 2,
+            name: 'cook',
+            status: 'done'
+        }
+    ]
+    res.json({data: todos})
+})
+
+app.get('/todo/:todo', (req, res) => {
+    res.json({
+        id: 1,
+        name: 'clean up',
+        status: 'open'
+    })
+})
+
+module.exports = app
